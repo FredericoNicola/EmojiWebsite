34f8859b4dd7717cf7939de8fb20691992c590fd
JoaoPauloAzevedo <61556966+JoaoPauloAzevedo@users.noreply.github.com>
2020-04-29 18:17:59 +0100

    footer

--- Emoji/Js/java.js
+++ Emoji/Js/java.js
@@ -1,29 +1,38 @@
-
 //função do botao traduzir
-function traduzir(){
-    var texto1 = document.getElementById("textoint").value;
-    var texto = texto1.toLocaleLowerCase();
-    
-    //warning message
-    if(texto == ""){
-        alert("Introduza pelo menos uma palavra")
-    }
-    else if(texto == "rir"){
-        document.getElementById("feito").innerHTML = "😁";
-    }
+function traduzir() {
+  var texto1 = document.getElementById("textoint").value;
+  var texto = texto1.toLocaleLowerCase();
 
-    else if(texto =="chorar"){
-        document.getElementById("feito").innerHTML = "😭";
-    }
-    else{
-        alert("Palavra Indisponivel para tradução");
-    }
+  //warning message
+  if (texto == "") {
+    document.getElementById("errorbox").style.display = "block";
+  } else if (texto == "rir") {
+    document.getElementById("feito").innerHTML = "😁";
+  } else if (texto == "chorar") {
+    document.getElementById("feito").innerHTML = "😭";
+  } else {
+    document.getElementById("errorbox-3").style.display = "block";
+  }
+}
 
+function fechar() {
+  document.getElementById("errorbox").style.display = "none";
+  document.getElementById("errorbox-2").style.display = "none";
+  document.getElementById("errorbox-3").style.display = "none";
 }
 
 //funçao do botao apagar
-function apagar(){
-    document.getElementById("textoint").value="";
-    document.getElementById("feito").value="";
+function apagar() {
+  document.getElementById("textoint").value = "";
+  document.getElementById("feito").value = "";
+  document.getElementById("errorbox-2").style.display = "block";
+}
 
+function navbar() {
+  var x = document.getElementById("mynav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
 }
