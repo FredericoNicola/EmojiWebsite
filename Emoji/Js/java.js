d419d3c8ce1d28f3a79b19977401142fcd2e45dc
FredericoNicola <fredericonicola@gmail.com>
2020-06-03 20:47:13 +0100

    update textbox
    update da textbox e da outbox para caber texto infinito

--- Emoji/Js/java.js
+++ Emoji/Js/java.js
@@ -1,6 +1,5 @@
 //função do botao traduzir
 
-
 // function traduzir() {
 //   var texto1 = document.getElementById("textoint").value;
 //   var texto = texto1.toLocaleLowerCase();
@@ -17,44 +16,28 @@
 //   }
 // }
 
-
-
-
-function traduzir(){
-
+function traduzir() {
   var texto1 = document.getElementById("textoint").value;
   var texto = texto1.toLocaleLowerCase();
-  textolista=texto.split(" ");
+  textolista = texto.split(" ");
 
-
-  for(i=0; i<textolista.length; i++){
-
-    if(textolista[i] == ""){
+  for (i = 0; i < textolista.length; i++) {
+    if (textolista[i] == "") {
       // alert("nada");
       document.getElementById("errorbox").style.display = "block";
-
-    }
-
-    else if(textolista[i] == "rir"){
+    } else if (textolista[i] == "rir") {
       // alert(textolista[i]);
-      document.getElementById("feito").innerHTML += "😁"+" ";
-    }
-
-    else if(textolista[i] == "chorar"){
+      document.getElementById("feito").innerHTML += "😁" + " ";
+    } else if (textolista[i] == "chorar") {
       // alert(textolista[i]);
-      document.getElementById("feito").innerHTML += "😭"+" ";
-      
-    }
-    else{
+      document.getElementById("feito").innerHTML += "😭" + " ";
+    } else {
       // alert(textolista[i]);
-      document.getElementById("feito").innerHTML+=textolista[i]+" ";
-      document.getElementById("errorbox-3").style.display ="block"
-
+      document.getElementById("feito").innerHTML += textolista[i] + " ";
+      document.getElementById("errorbox-3").style.display = "block";
     }
   }
 }
-
-
 
 function fechar() {
   document.getElementById("errorbox").style.display = "none";
