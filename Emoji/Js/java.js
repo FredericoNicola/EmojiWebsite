0193b86de5b76f6985f839564f51d8d11800f7b1
JoaoPauloAzevedo <61556966+JoaoPauloAzevedo@users.noreply.github.com>
2020-06-03 20:59:03 +0100

    bug da funtion traduzir
    bug corrigido

--- Emoji/Js/java.js
+++ Emoji/Js/java.js
@@ -391,44 +391,10 @@ function traduzir(){
       
     }
     else{
-// function traduzir() {
-//   var texto1 = document.getElementById("textoint").value;
-//   var texto = texto1.toLocaleLowerCase();
-
-//   warning message
-//   if (texto == "") {
-//     document.getElementById("errorbox").style.display = "block";
-//   } else if (texto == "rir") {
-//     document.getElementById("feito").innerHTML = "😁";
-//   } else if (texto == "chorar") {
-//     document.getElementById("feito").innerHTML = "😭";
-//   } else {
-//     document.getElementById("errorbox-3").style.display = "block";
-//   }
-// }
-
-function traduzir() {
-  var texto1 = document.getElementById("textoint").value;
-  var texto = texto1.toLocaleLowerCase();
-  textolista = texto.split(" ");
-
-  for (i = 0; i < textolista.length; i++) {
-    if (textolista[i] == "") {
-      // alert("nada");
-      document.getElementById("errorbox").style.display = "block";
-    } else if (textolista[i] == "rir") {
-      // alert(textolista[i]);
-      document.getElementById("feito").innerHTML += "😁" + " ";
-    } else if (textolista[i] == "chorar") {
-      // alert(textolista[i]);
-      document.getElementById("feito").innerHTML += "😭" + " ";
-    } else {
-      // alert(textolista[i]);
       document.getElementById("feito").innerHTML += textolista[i] + " ";
       document.getElementById("errorbox-3").style.display = "block";
     }
   }
-  //                                       alert(textolista);
 }
 
 function fechar() {
